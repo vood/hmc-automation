fe8dfacd2b6adf08259288124068e8d47153e1a1
Artem Vysotsky <artem.vysotsky@gmail.com>
2014-05-07 11:58:30 -0700

    Fixes db2

--- puppet/modules/db2/manifests/init.pp
+++ puppet/modules/db2/manifests/init.pp
@@ -1,21 +1,25 @@
 class db2 {
-  exec { '/usr/bin/wget http://10.8.7.188/v10.5fp1_linuxx64_expc.tar.gz':
-    command => '/usr/bin/wget "http://10.8.7.188/v10.5fp1_linuxx64_expc.tar.gz -O /tmp/ibm.tar.gz',
-    creates => '/tmp/ibm.tar.gz'
+  $tmp = "/tmp/ibmdb2expc"
+  file { "$tmp":
+    ensure => "directory"
   }->
-  file { '/tmp/db2expc.rsp':
-    content => template('db2/db2expc.rsp.erb')
+  exec { "/usr/bin/wget http://10.8.7.188/v10.5fp1_linuxx64_expc.tar.gz":
+    command => "/usr/bin/wget http://10.8.7.188/v10.5fp1_linuxx64_expc.tar.gz -O $tmp/ibm.tar.gz",
+    creates => "$tmp/ibm.tar.gz"
   }->
-  exec { '/usr/bin/tar zxf /tmp/ibm.tar.gz':
-    command => '/usr/bin/tar zxf /tmp/ibm.tar.gz',
-    creates => '/tmp/ibm/db2setup.sh'
+  file { "$tmp/db2expc.rsp":
+    content => template("db2/db2expc.rsp.erb")
   }->
-  exec { '/tmp/ibm/db2setup -f sysreq -r /tmp/db2expc.rsp':
-    command => '/tmp/ibm/db2setup -f sysreq -r /tmp/db2expc.rsp',
+  exec { "/bin/tar zxf $tmp.tar.gz -C /tmp":
+    command => "/bin/tar zxf ibm.tar.gz /tmp",
+    creates => "$tmp/db2setup.sh"
+  }->
+  exec { "$tmp/db2setup -f sysreq -r $tmp/db2expc.rsp":
+    command => "$tmp/db2setup -f sysreq -r $tmp/db2expc.rsp",
     timeout => 1800
   }->
-  file { '/home/db2inst1/sqllib/adm/.fenced':
-    owner => 'db2inst1',
-    group => 'db2iadm1'
+  file { "/home/db2inst1/sqllib/adm/.fenced":
+    owner => "db2inst1",
+    group => "db2iadm1"
   }
 }
