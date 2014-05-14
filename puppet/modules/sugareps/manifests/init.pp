7bfeadf46dd41866996e0c62a1b259adce96c974
Artem Vysotsky <artem.vysotsky@gmail.com>
2014-05-14 11:52:19 -0700

    pear Log module

--- puppet/modules/sugareps/manifests/init.pp
+++ puppet/modules/sugareps/manifests/init.pp
@@ -1,29 +1,30 @@
 class sugareps {
 
+  $php_package = params_lookup('php_package', 'global')
+  $mysql_package_param = params_lookup('mysql_package', 'global')
+
   file { '/etc/motd':
     content => "SugarEPS: PHP 5.3.x, IBM DB2 10.5, Apache 2.4.x\n\n"
   }
 
-  $mysql_package_param = params_lookup('mysql_package', 'global')
   $mysql_package = $mysql_package_param ? {
     '' => false,
     default => $mysql_package_param
   }
 
   class { 'devops':
-    php_package     => 'php53u',
+    php_package     => $php_package,
     mysql_package   => $mysql_package,
     deploy_environment => 'vagrantdb2'
   }
 
-  # since we are not installing mysql and this is the db2 box, we should install db2, but we need to make sure that
-  # the apache and php are installed first class is installed first
   class { 'devops::db::db2':
     require => [Devops::Apache['devops_apache'], Devops::Php['devops_php']]
   }
 
   php::pear::module { "Log":
     repository => 'pear.php.net',
-    module_prefix => 'php53u-'
+    module_prefix => "${php_package}-",
+    require => Package[$php_package]
   }
 }
